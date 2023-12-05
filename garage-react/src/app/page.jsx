7d9d676509aa04c1ff36a2dcfcfeda327765d009
FernandoJMartins <julio8martins@gmail.com>
2023-12-04 23:02:02 -0300

    adicao de elementos STATE

--- garage-react/src/app/page.jsx
+++ garage-react/src/app/page.jsx
@@ -1,19 +1,26 @@
+'use client';
+
+
 // import Image from 'next/image'
 import './style.css'
 import Sidebar from './Sidebar.jsx'
 import Form from './Form.jsx'
 
+import { useState } from 'react';
 
 // import { pedidos } from '@/data/pedidos';
 
-export default function Home() {
+
+export default function Home(){
+  const [isShowForm] = useState(true);
+
+
   return(
     <>
     <Sidebar />
     <main></main>
     <Form />
     </>
-  )
-
+  );
 }
 
