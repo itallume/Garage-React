0585448a7e991b5f3e2937bdb1653f6655586e40
FernandoJMartins <julio8martins@gmail.com>
2023-12-08 11:25:01 -0300

    reacriação do React e exclusão do CSS

---
+++ Documents/react-projetoo/Garage-React/src/app/cliente/page.jsx
@@ -0,0 +1,11 @@
+import Sidebar from "../../../components/Sidebar"
+import ClienteForm from "../../../components/ClienteForm"
+
+export default function Cliente(){
+    return(
+    <>
+        <Sidebar></Sidebar>
+        <ClienteForm></ClienteForm>
+    </>
+    )
+}
