e431d6973b115d650c80ee9553ac508bae3c8e53
FernandoJMartins <julio8martins@gmail.com>
2023-12-08 11:28:29 -0300

    reacriação do React e exclusão do CSS

---
+++ Documents/react-projetoo/Garage-React/garage-react/src/app/pedidos/page.jsx
@@ -0,0 +1,11 @@
+
+import Sidebar from "../../../components/Sidebar"
+
+export default function Pedidos(){
+    return(
+    <>
+        <Sidebar></Sidebar>
+        <h1 class="justify-center center flex text-white">essas sao os Pedidos</h1>
+    </>
+    )
+}
